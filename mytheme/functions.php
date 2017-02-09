8358a75ccd24af5dff173d3a5bf9cd58c7665a00
Kevin LEDOYEN <Kevin.ledoyen@gmail.com>
2017-02-09 16:37:52 +0100

    Update functions.php

--- mytheme/functions.php
+++ mytheme/functions.php
@@ -1,24 +1,22 @@
 <?php
-add_action('widgets_init','zero_add_sidebar');
-function zero_add_sidebar(){
-	$before_widget = "<div class='widgetRight'>";
-	$after_widget = '</div>';
-	$before_title = '<h1>';
-	$after_title = '</h1>';
-    register_sidebar(array(
-        'id' => 'my_custom_zone',
-        'name' => 'colonne droite',
-        'description' => 'Apparait en sur la droite de la page',
-        'before_widget' => $before_widget,
-        'after_widget' => $after_widget,
-        'before_title' => $before_title,
-        'after_title' => $after_title
-    ));
-}
 
-add_action('init', 'zero_add_menu');
-function zero_add_menu()
-{
-    register_nav_menu('main_menu', 'Menu principal');
-}
+
+// Code à insérer au début du fichier functions.php
+require "/core/Autoloader.php";
+Core\Autoloader::register();
+
+use Core\MyClass; // ici une classe MyClass de test dans le namespace Core (situé à la racine de /Core)
+use Core\Html\OtherClass; // ici une classe de test dans le namespace Core\Html (Donc le dossier /Core/Html)
+
+new MyClass; // pour instancier MyClass : Sinon si on a pas mis les 'use' au dessus c'est new Core/MyClass;
+new OtherClass; // pour instancier OtherClass : Sinon si on a pas mis les 'use' au dessus c'est new Core/Html/OtherClass;
+
+// Fin code à insérer 
+
+
+/*
+
+ICI le reste de votre fichier functions.php, dans lequel vous pouvez accéder à vos classes
+
+*/
 ?>
